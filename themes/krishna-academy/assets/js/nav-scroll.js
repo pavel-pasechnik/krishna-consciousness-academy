02c82bec3d56076367b549b170eaf56405e49be9
Pavel <Pavel.Pasechnik@gmail.com>
2025-08-12 16:14:24 +0300

    added mobile menu

--- themes/krishna-academy/assets/js/nav-scroll.js
+++ themes/krishna-academy/assets/js/nav-scroll.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const nav = document.querySelector('.header-wrapper');
+  const mobile = document.querySelector('.wp-block-navigation__responsive-dialog');
 
   function updateNavScrollState() {
     requestAnimationFrame(() => {
       if (window.scrollY > 5) {
         nav.classList.add('scrolled');
+        mobile.classList.add('scrolled');
       } else {
         nav.classList.remove('scrolled');
+        mobile.classList.remove('scrolled');
       }
     });
   }
